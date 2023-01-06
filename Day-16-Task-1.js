b79faa26b574450cd92fa2a43014ee94031de9b0
Vijay <113192642+vijayramesh1996@users.noreply.github.com>
2023-01-07 00:41:44 +0530

    Create Day-16-Task-1.js

---
+++ Day-16-Task-1.js
@@ -0,0 +1,49 @@
+let a = document.getElementById("container")
+setTimeout(()=>{
+    a.innerHTML = 15;
+    setTimeout(()=>{
+        a.innerHTML = 14;  
+        setTimeout(()=>{
+            a.innerHTML = 13;  
+            setTimeout(()=>{
+                a.innerHTML = 12;  
+                setTimeout(()=>{
+                    a.innerHTML = 11;  
+                    setTimeout(()=>{
+                        a.innerHTML = 10;
+                        setTimeout(()=>{
+                            a.innerHTML = 9;  
+                            setTimeout(()=>{
+                                a.innerHTML = 8;  
+                                setTimeout(()=>{
+                                    a.innerHTML = 7;  
+                                    setTimeout(()=>{
+                                        a.innerHTML = 6;  
+                                        setTimeout(()=>{
+                                            a.innerHTML = 5;  
+                                            setTimeout(()=>{
+                                                a.innerHTML= 4;  
+                                                setTimeout(()=>{
+                                                    a.innerHTML = 3;  
+                                                    setTimeout(()=>{
+                                                        a.innerHTML = 2;  
+                                                        setTimeout(()=>{
+                                                            a.innerHTML = 1;  
+                                                            setTimeout(()=>{
+                                                                a.innerHTML = "Happy Independance day";  
+                                                            },1000)
+                                                        },1000)
+                                                    },1000)
+                                                },1000)
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)  
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
